f56b6c15ce1408405aaa1602dcc1d983b649456f
Eric Zhuo Chen <zyxirchen@outlook.com>
2023-09-11 17:09:54 +0800

    Update directory structure

---
+++ zypys/wow/sync.py
@@ -0,0 +1,6 @@
+"""
+This module sync my World of Warcraft settings.
+
+It compares the last modified time of my local game settings with the cloud
+backup, and do backup or restoration based on which one is newer.
+"""
