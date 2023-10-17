634d80b17218ce64f55a56e99d425c20875ede5d
Eric Zhuo Chen <zyxirchen@outlook.com>
2023-10-17 18:06:44 +0800

    Update WoW sync script

--- zypys/wow/sync.py
+++ zypys/wow/sync.py
@@ -5,142 +5,143 @@ It compares the last modified time of my local game settings with the cloud
 backup, and do backup or restoration based on which one is newer.
 """
 
-import logging
+import os
 import shutil
-import zipfile
+import subprocess
 from pathlib import Path
 from typing import List
 
-from tqdm import tqdm
 
-
-def _modified_time_single_file(path: Path) -> float:
-    return path.stat().st_mtime
-
-
-def _modified_time(path: Path) -> float:
-    """Get the last modified time of `path`.
-
-    If `p` is a directory, get the newest modified time of the files within.
-    """
-    if path.is_file():
-        return _modified_time_single_file(path)
-    elif path.is_dir():
-        mtimes = [_modified_time_single_file(path)]
-        for p in path.glob("**/*"):
-            mtimes.append(p.stat().st_mtime)
+def get_mtime(p: Path) -> float:
+    """Get the newest modified time under a tree."""
+    if p.exists():
+        mtimes = [p.stat().st_mtime]
+        if p.is_dir():
+            for sub in p.iterdir():
+                mtimes.append(get_mtime(sub))
         return max(mtimes)
     else:
         return 0
 
 
-def modified_time(path: Path, dirs: List[str] = []) -> float:
-    """Get the newest modified time of `path` and the files within.
+def compress(main_path: Path, dirs: List[str], archive_path: Path):
+    """Compress `dirs` in `main_path` into `archive_path`.
 
-    If `dirs` is not empty, get the newest modified time of these subdirectories
-    instead.
+    This requires that one of "7-zip" or "NanaZip" is installed.
     """
-    if len(dirs):
-        def subdir_modified_time(d):
-            subdir = path.joinpath(d)
-            return _modified_time(subdir) if subdir.is_dir() else 0
-        return max(map(subdir_modified_time, dirs))
-    else:
-        return _modified_time(path)
+    # Change directory.
+    orig_dir = os.getcwd()
+    os.chdir(main_path)
+    # Compress with the "7z" command.
+    cmd = ["7z", "a", "-t7z", str(archive_path)] + dirs
+    proc = subprocess.Popen(
+        cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True
+    )
+    stdout = proc.stdout
+    if stdout is not None:
+        for line in stdout:
+            print(f"[7z] {line}")
+    proc.wait()
+    # Return to the original directory.
+    os.chdir(orig_dir)
 
 
-def make_archive(main_path: Path, dirs: List[str], archive_path: Path):
-    """Compress `dirs` in `main_path` into an archive."""
-    with zipfile.ZipFile(archive_path, "w", compression=zipfile.ZIP_LZMA) as zf:
-        # Add everything needed into the archive.
-        for d in dirs:
-            subdir = main_path.joinpath(d)
-            paths = subdir.glob("**/*")
-            path_sizes = map(lambda p: p.stat().st_size, paths)
-            with tqdm(total=sum(path_sizes), unit="bit") as pbar:
-                for path, size in zip(paths, path_sizes):
-                    if path.is_file():
-                        relative_path = path.relative_to(main_path)
-                        zf.write(path, arcname=relative_path)
-                        pbar.update(size)
+def extract(archive_path: Path, main_path: Path):
+    """Extract compressed archive `archive_path` to `main_path`."""
+    # Change directory.
+    orig_dir = os.getcwd()
+    os.chdir(main_path)
+    # Extract with the "7z" command.
+    cmd = ["7z", "x", str(archive_path)]
+    proc = subprocess.Popen(
+        cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True
+    )
+    stdout = proc.stdout
+    if stdout is not None:
+        for line in stdout:
+            print(f"[7z] {line}", end="")
+    proc.wait()
+    # Return to the original directory.
+    os.chdir(orig_dir)
 
 
 def backup(game_path: Path, dirs: List[str], backup_path: Path):
     """Backup directories under game path into a single archive."""
-    # Rename backup if already exists.
     if backup_path.exists():
+        print("Backing up the old backup...")
         new_name = backup_path.stem + "_old" + backup_path.suffix
         new_path = backup_path.parent.joinpath(new_name)
         new_path.unlink(missing_ok=True)
         backup_path.rename(new_path)
-    # Make the archive.
-    make_archive(game_path, dirs, backup_path)
+    print("Creating the new backup...")
+    compress(game_path, dirs, backup_path)
+    print("Work complete.")
 
 
 def restore(game_path: Path, dirs: List[str], backup_path: Path):
     """Restore backup into the game path."""
-    # Backup relevant directories if already exists.
     if any(map(lambda d: game_path.joinpath(d).exists, dirs)):
+        print("Backing up game settings...")
         new_name = backup_path.stem + "_old" + backup_path.suffix
         new_path = game_path.joinpath(new_name)
         new_path.unlink(missing_ok=True)
-        make_archive(game_path, dirs, new_path)
+        compress(game_path, dirs, new_path)
         for d in dirs:
             subdir = game_path.joinpath(d)
             shutil.rmtree(subdir, ignore_errors=True)
-    # Restore the archive.
-    with zipfile.ZipFile(backup_path, "r") as zf:
-        zf.extractall(game_path)
+    print("Restoring backup...")
+    extract(backup_path, game_path)
+    print("Work complete.")
+
+
+def do_nothing(*args):
+    """Ignore arguments and do nothing."""
+    pass
 
 
 def sync(game_path: Path, dirs: List[str], backup_path: Path) -> int:
     """Do backup or restore based on modified time."""
     # Check for game path.
     if not game_path.exists():
-        logging.error(f'Game path "{game_path}" does not exist!')
+        print(f'Game path "{game_path}" does not exist!')
         return 1
     # Decide an action.
     if not backup_path.exists():
-        action = "backup"
+        default_action = "backup"
     else:
-        game_modified_time = modified_time(game_path, dirs)
-        backup_modified_time = modified_time(backup_path)
-        if game_modified_time > backup_modified_time:
+        game_mtime = max(map(lambda d: get_mtime(game_path.joinpath(d)), dirs))
+        backup_mtime = get_mtime(backup_path)
+        if game_mtime > backup_mtime:
             default_action = "backup"
-        elif game_modified_time < backup_modified_time:
+        elif game_mtime < backup_mtime:
             default_action = "restore"
         else:
             default_action = "do nothing"
-        answer = input(f"I am going to {default_action}, shall I proceed? [Y/n] ")
-        while answer not in ["", "y", "Y", "n", "N"]:
-            answer = input("Please answer [y] or [n]: ")
-        if answer in ["", "y", "Y"]:
-            action = default_action
+    answer = input(f"I am going to {default_action}, shall I proceed? [Y/n] ")
+    while answer not in ["", "y", "Y", "n", "N"]:
+        answer = input("Please answer [y] or [n]: ")
+    if answer in ["", "y", "Y"]:
+        action = default_action
+    else:
+        answer = input("Then what should I do instead? ([B]ackup/[R]estore/[N]othing) ")
+        if answer in ["b", "B"]:
+            action = "backup"
+        elif answer in ["r", "R"]:
+            action = "restore"
         else:
-            answer = input("Then what should I do instead? [B]ackup/[R]estore/[N]othing")
-            if answer in ["b", "B"]:
-                action = "backup"
-            elif answer in ["r", "R"]:
-                action = "restore"
-            else:
-                action = "do nothing"
+            action = "do nothing"
 
     # Do an action.
-    if action == "backup":
-        logging.info("Backing up game settings.")
-        backup(game_path, dirs, backup_path)
-        logging.info("Work complete.")
-    elif action == "restore":
-        logging.info("Restoring game settings.")
-        restore(game_path, dirs, backup_path)
-        logging.info("Work complete.")
-    else:
-        logging.info("Nothing to do.")
+    action_dict = {
+        "backup": backup,
+        "restore": restore,
+    }
+    fn = action_dict.get(action, do_nothing)
+    fn(game_path, dirs, backup_path)
     return 0
 
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO)
     main_path = Path("c:/Program Files (x86)/World of Warcraft/_retail_/")
     dirs = ["Interface", "WTF"]
     archive_path = Path("c:/Users/zyxir/Zybox/wowui.zip")
